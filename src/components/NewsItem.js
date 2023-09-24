e7e58fa6c74757eed151750f30f0cb3559d32115
Kamrul Islam Nayeem <naeem8541@gmail.com>
2023-09-25 04:31:44 +0600

    update news file with axios api instead of fetch api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,14 +4,14 @@ export class NewsItem extends Component {
 
     
     render() {
-        let {title, description , ImageUrl, readMore , alt} = this.props
+        let {title, description , ImageUrl, readMore , alt , newsUrl} = this.props
         return (
-            <div className="card" style={{ width: "22rem" }}>
-                <img src={ImageUrl} className="card-img-top" alt={alt}/>
+            <div className="card my-3">
+                <img src={!ImageUrl ? "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg" : ImageUrl} className="card-img-top img-fluid h-50" alt={alt}/>
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <a href="/" className="btn btn-primary">{readMore}</a>
+                        <p className="card-text">{description}...</p>
+                        <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-primary">{readMore}</a>
                     </div>
             </div>
         )
